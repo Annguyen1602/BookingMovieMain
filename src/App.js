33f88110577e5124001c3e9983107a0151d15e24
annguyen <nguyentruongan16021996@gmail.com>
2022-09-06 19:44:23 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import DemoTailwind from "./DemoTailwind/DemoTailwind";
+
 
 function App() {
   return (
     <div className="App">
-      <DemoTailwind/>
+      
       
     </div>
   );
